4494539884e534fce1dd526b96948a23281e9c92
yangwenyi <yangwenyi@outlook.com>
2019-03-07 15:53:20 +0800

    add fonts

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,7 +6,9 @@ export default class Home extends React.Component {
     render() {
         return (
             <div className='home'>
-
+                <div className='home-slogan'>
+                    <div className='home-slogan-text'>Building a smarter world bases on SDAG</div>
+                </div>
             </div>
         );
     }
