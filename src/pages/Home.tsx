7d321fc999a13c051a02ee79e717ca5c2372ffcd
yangwenyi <yangwenyi@outlook.com>
2019-03-07 18:01:45 +0800

    save

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,10 @@
 import * as React from 'react';
 import './Home.css';
 
+import thunder from '../assets/thunder.svg';
+import scalable from '../assets/scalable.svg';
+import usable from '../assets/usable.svg';
+
 export default class Home extends React.Component {
 
     render() {
@@ -9,6 +13,26 @@ export default class Home extends React.Component {
                 <div className='home-slogan'>
                     <div className='home-slogan-text'>Building a smarter world bases on SDAG</div>
                 </div>
+
+                <div className='home-intro'>
+                    <div>Advantages</div>
+
+                    <div className='home-intro-items'>
+                        <div>
+                            <img src={thunder} />
+                            <div>Fast</div>
+                        </div>
+                        <div>
+                            <img src={scalable} />
+                            <div>Scalable</div>
+                        </div>
+                        <div>
+                            <img src={usable} />
+                            <div>Usable</div>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         );
     }
