ce6eab609c17229036e172791cb3eadca010860b
yangwenyi <yangwenyi@outlook.com>
2019-03-20 10:35:50 +0800

    add overlay menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,55 @@
 import * as React from 'react';
 import './App.css';
-import { Header ,Footer} from './components';
+import { Header, Footer } from './components';
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import { Home } from './pages';
+import HamburgerMenu from 'react-hamburger-menu';
+import * as jQuery from 'jquery';
 
 class App extends React.Component {
+
+  header: Header;
+
+  private openMenu() {
+    jQuery('#overlay-menu').css('display', 'block');
+    jQuery('#overlay-menu').animate({ 'height': jQuery(document.body).height(), 'opacity': 1 });
+  }
+
+  private closeMenu() {
+    jQuery('#overlay-menu').animate({ 'opacity': 0 }, 500, () => jQuery('#overlay-menu').css('display', 'none'));
+  }
+
   public render() {
     return (
       <Router >
         <div className='App'>
-          <Header />
+          <Header ref={e => this.header = e!} onMenuOpen={() => this.openMenu()} onMenuClose={() => this.closeMenu()} />
 
           <Switch>
             <Route path='/' component={Home} />
           </Switch>
 
           <Footer />
+
+          <div id='overlay-menu'>
+            <div id='overlay-menu-list'>
+              <ul>
+                <li>
+                  <a href="#whitepaper" onClick={() => this.header.closeMenu()}>WhitePaper</a>
+                </li>
+                <li>
+                  <a href="#developers" onClick={() => this.header.closeMenu()}>Developers</a>
+                </li>
+                <li>
+                  <a href="#products" onClick={() => this.header.closeMenu()}>Products</a>
+                </li>
+                <li>
+                  <a href="#about" onClick={() => this.header.closeMenu()}>About</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+
         </div>
       </Router>
     );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,12 @@ interface HeaderState {
     isOpen: boolean;
 }
 
-export default class Header extends React.Component<any, HeaderState>{
+interface HeaderProps {
+    onMenuOpen?: () => void;
+    onMenuClose?: () => void;
+}
+
+export default class Header extends React.Component<HeaderProps, HeaderState>{
 
     private header: HTMLHeadElement;
     private title: HTMLDivElement;
@@ -16,11 +21,23 @@ export default class Header extends React.Component<any, HeaderState>{
         window.addEventListener('scroll', ev => this.onScroll(ev));
     }
 
+    private toggleMenu() {
+        let open = !this.state.isOpen;
+        this.setState({ isOpen: open });
+        if (this.props.onMenuOpen && open) this.props.onMenuOpen();
+        if (this.props.onMenuClose && !open) this.props.onMenuClose();
+    }
+
+    closeMenu() {
+        this.setState({ isOpen: false });
+        if (this.props.onMenuClose) this.props.onMenuClose();
+    }
+
     private onScroll(ev: UIEvent) {
         const shadowClass = 'header-shadow';
         const titleScaleClass = 'header-title-small';
 
-        if (!window.scrollY) {
+        if (window.scrollY <= 0) {
             this.header.classList.remove(shadowClass)
             this.title.classList.remove(titleScaleClass)
             return;
@@ -37,7 +54,7 @@ export default class Header extends React.Component<any, HeaderState>{
 
     render() {
         return (
-            <header ref={e => this.header = e as HTMLHeadElement}>
+            <header ref={e => this.header = e as HTMLHeadElement} style={{ backgroundColor: this.state.isOpen ? 'rgba(0, 0, 0, 0)' : 'rgba(255, 255, 255, 0.85)' }}>
                 <div ref={e => this.title = e as HTMLDivElement} className='header-title'>SDAG</div>
                 <div className='header-links'>
                     <a href="#whitepaper">WhitePaper</a>
@@ -49,12 +66,12 @@ export default class Header extends React.Component<any, HeaderState>{
                 <div id='menu'>
                     <HamburgerMenu
                         isOpen={this.state.isOpen}
-                        menuClicked={() => { this.setState({ isOpen: !this.state.isOpen }) }}
+                        menuClicked={() => this.toggleMenu()}
                         width={18}
                         height={15}
                         strokeWidth={1}
                         rotate={0}
-                        color='black'
+                        color={this.state.isOpen ? 'white' : 'black'}
                         borderRadius={0}
                         animationDuration={0.5}
                     />
